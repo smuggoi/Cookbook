6fb8986d07471126ae780d1c2cb3327370a09b09
smuggoi <44080359+smuggoi@users.noreply.github.com>
2018-10-11 22:48:11 -0700

    Adding user input and variable type examples

---
+++ user_input.py
@@ -0,0 +1,7 @@
+# print("Hello")
+# name = input("What is your name? ")
+# print("Hello " + name)
+
+number = input("What is your favorite number? ")
+triple = int(number) * 3        # Type cast string to int
+print("Your number multiplied by three is " + str(triple))  # Type cast int to string
---
+++ variable_types.py
@@ -0,0 +1,10 @@
+my_integer = 3              # Declaration and initialization of integer
+my_float = 3.141592         # Declaration and initialization of float
+my_string = "hello"         # Declaration and initialization of string
+
+print(type(my_integer))
+print(type(my_float))
+print(type(my_string))
+
+my_integer = "some text"
+print(type(my_integer))     # Variables aren't locked to a single type
